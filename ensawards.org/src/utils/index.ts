049a8a4dbff89113f5e0a95de945729a409e8db6
y3drk <ziebura.jedrzej@gmail.com>
2025-08-05 15:40:11 +0200

    Styling for desktop - pt.2 => implementation of the rest of the styles and 404 page

--- ensawards.org/src/utils/index.ts
+++ ensawards.org/src/utils/index.ts
@@ -1,6 +1,6 @@
 export const adaptBreadcrumb = (breadcrumb: string): string =>
-  breadcrumb.toLowerCase().replace(" ", "-");
-//If we only use it in subpage hero, maybe it's best to move it there
+  breadcrumb.toLowerCase().replaceAll(" ", "-");
+//TODO: If we only use it in subpage hero, maybe it's best to move it there
 
 export const adaptSuggestionText = (whatsSuggested: "app" | "best practice"): string => {
   switch (whatsSuggested) {
