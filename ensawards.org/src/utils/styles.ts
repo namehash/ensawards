8f90caaba12f3241ed3ffc0eb6bd994e4758d23a
y3drk <ziebura.jedrzej@gmail.com>
2025-08-29 12:21:16 +0200

    formatting and linting

--- ensawards.org/src/utils/styles.ts
+++ ensawards.org/src/utils/styles.ts
@@ -1,10 +1,10 @@
-import {BenchmarkResult} from "@/data/appData.ts";
+import { BenchmarkResult } from "@/data/appData.ts";
 
 export const getAppSupportColor = (value: number): string =>
-    value > 75 ? "text-emerald-600" : value > 35 ? "text-amber-600" : "text-red-600";
+  value > 75 ? "text-emerald-600" : value > 35 ? "text-amber-600" : "text-red-600";
 
 export const BenchmarkResultToBadgeStyles = new Map<BenchmarkResult, string>([
-    [BenchmarkResult.PartialPass, "text-neutral-900 bg-neutral-100"],
-    [BenchmarkResult.Pass, "text-emerald-600 bg-[#0596691A]"],
-    [BenchmarkResult.Fail, "text-red-600 bg-[#DC26261A]"],
+  [BenchmarkResult.PartialPass, "text-neutral-900 bg-neutral-100"],
+  [BenchmarkResult.Pass, "text-emerald-600 bg-[#0596691A]"],
+  [BenchmarkResult.Fail, "text-red-600 bg-[#DC26261A]"],
 ]);
