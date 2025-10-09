2c12d8f70fed691c4c3b0fdc340ba34dc5d9fbb4
y3drk <ziebura.jedrzej@gmail.com>
2025-10-09 16:23:32 +0200

    Application of Google Doc 10/08/25 suggestions --> items 1-4

--- ensawards.org/src/utils/chains.ts
+++ ensawards.org/src/utils/chains.ts
@@ -7,6 +7,7 @@ import {
   scroll,
 } from "viem/chains";
 import type {ChainId} from "@ensnode/ensnode-sdk";
+import type {Address} from "viem";
 
 export const SUPPORTED_CHAINS = [
   mainnet,
@@ -38,3 +39,35 @@ export function getChainName(chainId: ChainId): string {
   const name = CUSTOM_CHAIN_NAMES.get(chainId);
   return name || `Unknown Chain (${chainId})`;
 }
+
+/**
+ * Gets the base block explorer URL for a given chainId
+ *
+ * @returns default block explorer URL for the chain with the provided id,
+ * or null if the referenced chain doesn't have a known block explorer
+ */
+export function getChainBlockExplorerUrl(chainId: ChainId): URL | null {
+  const chain = SUPPORTED_CHAINS.find((chain) => chain.id === chainId);
+  // if ENSAwards doesn't support a chain with a given id
+  // (theoretically should never happen)
+  if (!chain) return null;
+
+  // if a supported chain does not have a blockExplorer
+  // (theoretically should never happen)
+  if (!chain.blockExplorers) return null;
+
+  return new URL(chain.blockExplorers.default.url);
+}
+
+/**
+ * Gets the block explorer URL for a specific contract on a specific chainId
+ *
+ * @returns complete block explorer URL for a specific contract on a specific chainId,
+ * or null if the referenced chain doesn't have a known block explorer
+ */
+export const getBlockExplorerUrlForContract = (chainId: ChainId, contractAddress: Address): URL | null => {
+  const chainBlockExplorer = getChainBlockExplorerUrl(chainId);
+  if (!chainBlockExplorer) return null;
+
+  return new URL(contractAddress, chainBlockExplorer.toString());
+};
