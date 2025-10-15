e4868f970f9987f0f746f3c54c91914cc5ddbb8f
y3drk <ziebura.jedrzej@gmail.com>
2025-10-15 16:28:25 +0200

    Creation of new, dedicated <ContractBadge> component + new directory for all project badges + new <GenericTooltip> component over shadcn tooltip base component

--- ensawards.org/src/components/atoms/SmartContractMetadata.tsx
+++ ensawards.org/src/components/atoms/SmartContractMetadata.tsx
@@ -1,13 +1,9 @@
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipProvider,
-  TooltipTrigger,
-} from "@/components/ui/tooltip.tsx";
 import type { ContractMetadata } from "@/types/contracts.ts";
 import { cn } from "@/utils/tailwindClassConcatenation.ts";
 import { Aperture, BadgeDollarSign, Bell, Citrus } from "lucide-react";
 import React from "react";
+import {GenericTooltip} from "@/components/atoms/GenericTooltip.tsx";
+import {TooltipProvider} from "@/components/ui/tooltip.tsx";
 
 export interface SmartContractMetadataProps {
   metadata: ContractMetadata;
@@ -20,70 +16,38 @@ const getDeactivatedMetadataFieldMessage = () => "Example deactivated info";
 
 export function SmartContractMetadata({ metadata }: SmartContractMetadataProps) {
   return (
-    <TooltipProvider delayDuration={1000} skipDelayDuration={0}>
+      <TooltipProvider delayDuration={1000} skipDelayDuration={0}>
       <div className="flex flex-row flex-nowrap justify-start items-center gap-3">
         {/*TODO replace with actual appropriate icons when they are provided*/}
-        <Tooltip>
-          <TooltipTrigger asChild>
-            <Aperture
-              className={cn("w-[21px] h-[21px]", metadata.field1 ? "text-black" : "text-gray-200")}
-            />
-          </TooltipTrigger>
-          <TooltipContent
-            sideOffset={10}
-            className="bg-gray-50 text-sm text-black shadow-md outline-none max-w-[275px]"
-          >
-            {metadata.field1
+        <GenericTooltip content={<>{metadata.field1
               ? getActivatedMetadataFieldMessage(metadata.field1)
-              : getDeactivatedMetadataFieldMessage()}
-          </TooltipContent>
-        </Tooltip>
-        <Tooltip>
-          <TooltipTrigger asChild>
-            <Bell
+            : getDeactivatedMetadataFieldMessage()}</>} >
+          <Aperture
+              className={cn("w-[21px] h-[21px]", metadata.field1 ? "text-black" : "text-gray-200")}
+          />
+        </GenericTooltip>
+        <GenericTooltip content={<>{metadata.field2
+            ? getActivatedMetadataFieldMessage(metadata.field2)
+            : getDeactivatedMetadataFieldMessage()}</>} >
+          <Bell
               className={cn("w-[21px] h-[21px]", metadata.field2 ? "text-black" : "text-gray-200")}
-            />
-          </TooltipTrigger>
-          <TooltipContent
-            sideOffset={10}
-            className="bg-gray-50 text-sm text-black shadow-md outline-none max-w-[275px]"
-          >
-            {metadata.field2
-              ? getActivatedMetadataFieldMessage(metadata.field2)
-              : getDeactivatedMetadataFieldMessage()}
-          </TooltipContent>
-        </Tooltip>
-        <Tooltip>
-          <TooltipTrigger asChild>
-            <Citrus
+          />
+        </GenericTooltip>
+        <GenericTooltip content={<>{metadata.field3
+            ? getActivatedMetadataFieldMessage(metadata.field3)
+            : getDeactivatedMetadataFieldMessage()}</>} >
+          <Citrus
               className={cn("w-[21px] h-[21px]", metadata.field3 ? "text-black" : "text-gray-200")}
-            />
-          </TooltipTrigger>
-          <TooltipContent
-            sideOffset={10}
-            className="bg-gray-50 text-sm text-black shadow-md outline-none max-w-[275px]"
-          >
-            {metadata.field3
-              ? getActivatedMetadataFieldMessage(metadata.field3)
-              : getDeactivatedMetadataFieldMessage()}
-          </TooltipContent>
-        </Tooltip>
-        <Tooltip>
-          <TooltipTrigger asChild>
-            <BadgeDollarSign
+          />
+        </GenericTooltip>
+        <GenericTooltip content={<>{metadata.field4
+            ? getActivatedMetadataFieldMessage(metadata.field4)
+            : getDeactivatedMetadataFieldMessage()}</>} >
+          <BadgeDollarSign
               className={cn("w-[21px] h-[21px]", metadata.field4 ? "text-black" : "text-gray-200")}
-            />
-          </TooltipTrigger>
-          <TooltipContent
-            sideOffset={10}
-            className="bg-gray-50 text-sm text-black shadow-md outline-none max-w-[275px]"
-          >
-            {metadata.field4
-              ? getActivatedMetadataFieldMessage(metadata.field4)
-              : getDeactivatedMetadataFieldMessage()}
-          </TooltipContent>
-        </Tooltip>
+          />
+        </GenericTooltip>
       </div>
-    </TooltipProvider>
+      </TooltipProvider>
   );
 }
