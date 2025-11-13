036d72b80dda9b8af867179cb47b5c141d5563e8
y3drk <ziebura.jedrzej@gmail.com>
2025-11-13 16:34:02 +0100

    Formatting & linting (1)

--- ensawards.org/src/utils/index.ts
+++ ensawards.org/src/utils/index.ts
@@ -1,6 +1,6 @@
 import type { PossibleSuggestions } from "@/components/molecules/contact-form/types.ts";
 import type { UnixTimestamp } from "@ensnode/ensnode-sdk";
-import {getUnixTime} from "date-fns";
+import { getUnixTime } from "date-fns";
 
 /**
  * Checks whether a given string is a valid slug.
@@ -62,4 +62,6 @@ export const getSuggestionText = (whatsSuggested: PossibleSuggestions): string =
  * Date marking the approximate end of the ENS contract naming season event.
  * April 30th 2026 at 23:59:59 UTC
  */
-export const ENS_CONTRACT_NAMING_SEASON_APPROX_END: UnixTimestamp = getUnixTime(new Date("2026-04-30T23:59:59.000Z"));
+export const ENS_CONTRACT_NAMING_SEASON_APPROX_END: UnixTimestamp = getUnixTime(
+  new Date("2026-04-30T23:59:59.000Z"),
+);
