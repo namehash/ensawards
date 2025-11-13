8f15422374ae31686d9395242bf351aa29fc179c
Nischal Sharma <nischal@web3labs.com>
2025-11-13 17:52:54 +0530

    Aave DAO Governance contracts added
    Signed-off-by: Nischal Sharma <nischal@web3labs.com>

--- ensawards.org/src/data/contracts.ts
+++ ensawards.org/src/data/contracts.ts
@@ -510,9 +510,113 @@ export const CONTRACTS: Contract[] = [
     cachedIdentity: {
       resolutionStatus: ContractResolutionStatusIds.Unnamed,
       contract: {
-        address: "0x0000000000000000000000000000000000000003",
+        address: "0x9AEE0B04504CeF83A65AC3f0e838D0593BCb2BC7",
         chain: mainnet,
-        codeName: "TestContractAave",
+        codeName: "AaveGovernance",
+      },
+    },
+  },
+  {
+    org: AaveDaoOrg,
+    type: ContractTypes.Dao,
+    subtype: ContractSubtypes.Governance,
+    cachedIdentity: {
+      resolutionStatus: ContractResolutionStatusIds.Unnamed,
+      contract: {
+        address: "0xa198Fac58E02A5C5F8F7e877895d50cFa9ad1E04",
+        chain: mainnet,
+        codeName: "GovernancePowerStrategy",
+      },
+    },
+  },
+  {
+    org: AaveDaoOrg,
+    type: ContractTypes.Dao,
+    subtype: ContractSubtypes.Governance,
+    cachedIdentity: {
+      resolutionStatus: ContractResolutionStatusIds.Unnamed,
+      contract: {
+        address: "0x6ACe1Bf22D57a33863161bFDC851316Fb0442690",
+        chain: mainnet,
+        codeName: "VotingPortalEth",
+      },
+    },
+  },
+  {
+    org: AaveDaoOrg,
+    type: ContractTypes.Dao,
+    subtype: ContractSubtypes.Governance,
+    cachedIdentity: {
+      resolutionStatus: ContractResolutionStatusIds.Unnamed,
+      contract: {
+        address: "0x06a1795a88b82700896583e123F46BE43877bFb6",
+        chain: mainnet,
+        codeName: "VotingMachine",
+      },
+    },
+  },
+  {
+    org: AaveDaoOrg,
+    type: ContractTypes.Dao,
+    subtype: ContractSubtypes.Governance,
+    cachedIdentity: {
+      resolutionStatus: ContractResolutionStatusIds.Unnamed,
+      contract: {
+        address: "0xA16044F268d84599e58E851b25f4DBA061DC6088",
+        chain: mainnet,
+        codeName: "VotingStrategy",
+      },
+    },
+  },
+  {
+    org: AaveDaoOrg,
+    type: ContractTypes.Dao,
+    subtype: ContractSubtypes.Governance,
+    cachedIdentity: {
+      resolutionStatus: ContractResolutionStatusIds.Unnamed,
+      contract: {
+        address: "0xa638c14525CAEE8e498A67b409CCb1B7b34c3981",
+        chain: mainnet,
+        codeName: "DataWarehouse",
+      },
+    },
+  },
+  {
+    org: AaveDaoOrg,
+    type: ContractTypes.Dao,
+    subtype: ContractSubtypes.Governance,
+    cachedIdentity: {
+      resolutionStatus: ContractResolutionStatusIds.Unnamed,
+      contract: {
+        address: "0x5300A1a15135EA4dc7aD5a167152C01EFc9b192A",
+        chain: mainnet,
+        codeName: "ExecutorLvl1",
+      },
+    },
+  },
+  {
+    org: AaveDaoOrg,
+    type: ContractTypes.Dao,
+    subtype: ContractSubtypes.Governance,
+    cachedIdentity: {
+      resolutionStatus: ContractResolutionStatusIds.Unnamed,
+      contract: {
+        address: "0x17Dd33Ed0e3dD2a80E37489B8A63063161BE6957",
+        chain: mainnet,
+        codeName: "ExecutorLvl2",
+      },
+    },
+  },
+  {
+    org: AaveDaoOrg,
+    type: ContractTypes.Dao,
+    subtype: ContractSubtypes.Governance,
+    cachedIdentity: {
+      resolutionStatus: ContractResolutionStatusIds.Unnamed,
+      contract: {
+        address: "0xdAbad81aF85554E9ae636395611C58F7eC1aAEc5",
+        chain: mainnet,
+        codeName: "PayloadsController",
       },
     },
   },
