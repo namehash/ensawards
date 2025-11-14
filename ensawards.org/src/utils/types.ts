a51ae8c6a7c84665d83114055637047d2ca18ea0
y3drk <ziebura.jedrzej@gmail.com>
2025-11-14 10:33:18 +0100

    Add quotes to 'Contract Naming Season' landing page -> Closes Issue #44

---
+++ ensawards.org/src/utils/types.ts
@@ -0,0 +1,10 @@
+export interface QuoteAuthorData {
+    name: string;
+    role: string;
+    ensProfile: string;
+}
+export interface QuoteCardData {
+    quote: string;
+    author: QuoteAuthorData;
+    avatarPath: string;
+}
