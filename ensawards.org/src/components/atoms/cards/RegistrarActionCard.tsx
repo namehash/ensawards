c42cd4f5c43047201fd243704b6474498bb52480
y3drk <ziebura.jedrzej@gmail.com>
2025-12-04 17:22:20 +0100

    RegistrarAction card styling, pt.1

--- ensawards.org/src/components/atoms/cards/RegistrarActionCard.tsx
+++ ensawards.org/src/components/atoms/cards/RegistrarActionCard.tsx
@@ -1,6 +1,6 @@
 import { memo, type PropsWithChildren } from "react";
 import { zeroAddress } from "viem";
-import { Info as InfoIcon } from "lucide-react";
+import { Info as InfoIcon, Check as CheckIcon, X as XIcon } from "lucide-react";
 import type {
     DefaultableChainId,
     ENSNamespaceId,
@@ -21,6 +21,7 @@ import {buildExternalEnsAppProfileUrl, getBlockExplorerUrlForTransactionHash} fr
 import {RelativeTime} from "@/components/atoms/datetime/RelativeTime.tsx";
 import {NameDisplay} from "@/components/atoms/identity/utils.tsx";
 import {DisplayDuration} from "@/components/atoms/datetime/DisplayDuration.tsx";
+import {useIsMobile} from "@/utils/hooks/useMobile.tsx";
 
 // TODO: Restyle all elements accordingly to Figma
 
@@ -34,7 +35,7 @@ interface LabeledFieldProps {
  */
 function LabeledField({ fieldLabel, className, children }: PropsWithChildren<LabeledFieldProps>) {
     return (
-        <div className={cn("flex flex-col flex-nowrap justify-start items-start", className)}>
+        <div className={cn("max-sm:w-full flex flex-row sm:flex-col flex-nowrap justify-between sm:justify-center items-start gap-0 max-sm:self-stretch", className)}>
             <p className="text-muted-foreground text-sm leading-normal font-normal">{fieldLabel}</p>
             {children}
         </div>
@@ -63,7 +64,7 @@ function ResolveAndDisplayReferrerIdentity({
         !isRegistrarActionReferralAvailable(referral) ||
         referral.encodedReferrer === ZERO_ENCODED_REFERRER
     ) {
-        return <>-</>;
+        return <p className="h-[21px]">-</p>;
     }
 
     // if the encoded referrer was not the zeroEncodedReferrer but couldn't be
@@ -97,7 +98,8 @@ function ResolveAndDisplayReferrerIdentity({
         <ResolveAndDisplayIdentity
             identity={referrerIdentity}
             namespaceId={namespaceId}
-            withAvatar={true}
+            withAvatar={false}
+            withTooltip={false}
             className="font-medium"
         />
     );
@@ -108,25 +110,31 @@ function ResolveAndDisplayReferrerIdentity({
  */
 export function RegistrarActionCardLoading() {
     return (
-        <div className="w-full min-h-[80px] box-border flex flex-row flex-wrap justify-between items-center max-lg:gap-3 gap-y-3 rounded-xl border p-3 text-sm">
-            <LabeledField fieldLabel="Name" className="w-[30%] min-w-[200px]">
-                <div className="animate-pulse mt-1 h-6 bg-muted rounded w-3/5" />
+        <div className="w-full min-h-[80px] box-border flex flex-col sm:flex-row flex-wrap justify-start sm:justify-between items-start sm:items-center gap-2 p-4 sm:p-6 sm:gap-y-5 rounded-2xl border border-gray-200 text-sm bg-white">
+            <LabeledField fieldLabel="Name" className="w-[30%] min-w-[192px]">
+                <div className="animate-pulse mt-1 h-6 bg-muted rounded-sm w-3/5" />
             </LabeledField>
 
             <LabeledField fieldLabel="Registered" className="w-[15%] min-w-[140px]">
-                <div className="animate-pulse mt-1 h-6 bg-muted rounded w-4/5" />
+                <div className="animate-pulse mt-1 h-6 bg-muted rounded-sm w-4/5" />
             </LabeledField>
 
             <LabeledField fieldLabel="Duration" className="w-[10%] min-w-[140px]">
-                <div className=" animate-pulse mt-1 h-6 bg-muted rounded w-4/5" />
+                <div className=" animate-pulse mt-1 h-6 bg-muted rounded-sm w-4/5" />
             </LabeledField>
 
-            <LabeledField fieldLabel="Registrant" className="w-1/5 min-w-[140px]">
-                <div className="animate-pulse mt-1 h-6 bg-muted rounded w-3/5" />
+            <LabeledField fieldLabel="Registrant" className="w-1/5 min-w-[192px]">
+                {/*TODO: Improve skeleton*/}
+                <div className="animate-pulse mt-1 h-6 bg-muted rounded-sm w-3/5" />
             </LabeledField>
 
             <LabeledField fieldLabel="Referrer" className="w-[15%]  min-w-[140px]">
-                <div className=" animate-pulse mt-1 h-6 bg-muted rounded w-full" />
+                <div className=" animate-pulse mt-1 h-6 bg-muted rounded-sm w-full" />
+            </LabeledField>
+
+            <LabeledField fieldLabel="ENS Holiday Awards" className="w-[15%] min-w-[140px]">
+                {/*TODO: Improve skeleton*/}
+                <div className=" animate-pulse mt-1 h-6 bg-muted rounded-sm w-full"/>
             </LabeledField>
         </div>
     );
@@ -144,6 +152,7 @@ export function RegistrarActionCard({
                                                namespaceId,
                                                namedRegistrarAction,
                                            }: RegistrarActionCardProps) {
+    const isMobile = useIsMobile();
     const {registrant, registrationLifecycle, type, referral, transactionHash} =
         namedRegistrarAction.action;
     const {chainId} = registrationLifecycle.subregistry.subregistryId;
@@ -152,7 +161,7 @@ export function RegistrarActionCard({
     const withTransactionLink = ({children}: PropsWithChildren) =>
         // wrap `children` content with a transaction link only if the URL is defined
         transactionDetailUrl ? (
-            <a target="_blank" href={transactionDetailUrl.toString()}>{children}</a>
+            <a target="_blank" className="w-fit text-blue-600 font-medium hover:underline hover:underline-offset-[25%]" href={transactionDetailUrl.toString()}>{children}</a>
         ) : (
             children
         );
@@ -160,14 +169,14 @@ export function RegistrarActionCard({
     const registrantIdentity = buildUnresolvedIdentity(registrant, namespaceId, chainId);
 
     return (
-        <div className="w-full min-h-[80px] box-border flex flex-row flex-wrap justify-between items-center max-lg:gap-3 gap-y-3 rounded-xl border p-3 text-sm">
-            <LabeledField fieldLabel="Name" className="w-[30%] min-w-[200px]">
-                <div className="w-full overflow-x-auto">
+        <div className="w-full min-h-[80px] box-border flex flex-col sm:flex-row flex-wrap justify-start sm:justify-between items-start sm:items-center gap-2 p-4 sm:p-6 sm:gap-y-5 rounded-2xl border border-gray-200 text-sm bg-white">
+            <LabeledField fieldLabel="Name" className="w-1/5 min-w-[192px]">
+                <div className="sm:w-full h-[21px] overflow-x-auto">
                     {/*TODO: Temporarily assuming that for the registered (renewed) name we'd want to link to ENSManagerApp */}
                     <a
                         target="_blank"
                         href={buildExternalEnsAppProfileUrl(namedRegistrarAction.name, namespaceId)?.href}
-                        className="inline-flex items-center gap-2 text-blue-600 hover:underline"
+                        className="w-fit text-blue-600 font-medium hover:underline hover:underline-offset-[25%]"
                     >
                         <NameDisplay name={namedRegistrarAction.name} />
                     </a>
@@ -178,26 +187,43 @@ export function RegistrarActionCard({
                 fieldLabel={type === RegistrarActionTypes.Registration ? "Registered" : "Renewed"}
                 className="w-[15%] min-w-[140px]"
             >
-                <RelativeTime
+                <p className="h-[21px]"><RelativeTime
                     timestamp={namedRegistrarAction.action.block.timestamp}
                     tooltipPosition="top"
                     conciseFormatting={true}
                     contentWrapper={withTransactionLink}
-                />
+                /></p>
             </LabeledField>
 
             <LabeledField fieldLabel="Duration" className="w-[10%]  min-w-[140px]">
-                <DisplayDuration duration={namedRegistrarAction.action.incrementalDuration} />
+                <p className="h-[21px] font-medium">
+                    <DisplayDuration duration={namedRegistrarAction.action.incrementalDuration}/>
+                </p>
             </LabeledField>
 
-            <LabeledField fieldLabel="Registrant" className="w-1/5 overflow-x-auto min-w-[140px]">
-                <ResolveAndDisplayIdentity
+            <div className="flex flex-row flex-nowrap justify-start items-center gap-3 max-sm:w-full w-1/5 min-w-[192px]">
+                {!isMobile && <ResolveAndDisplayIdentity
                     identity={registrantIdentity}
                     namespaceId={namespaceId}
+                    accelerate={true}
                     withAvatar={true}
+                    withTooltip={false}
+                    withIdentifier={false}
                     className="font-medium"
-                />
-            </LabeledField>
+                    avatarStyles="w-10 h-10"
+                />}
+                <LabeledField fieldLabel="Registrant" className="sm:min-w-[140px]">
+                    <ResolveAndDisplayIdentity
+                        identity={registrantIdentity}
+                        namespaceId={namespaceId}
+                        accelerate={true}
+                        withAvatar={isMobile}
+                        withTooltip={false}
+                        className="font-medium sm:max-w-[140px] sm:overflow-x-auto"
+                        avatarStyles="w-5 h-5"
+                    />
+                </LabeledField>
+            </div>
 
             <LabeledField fieldLabel="Referrer" className="w-[15%] min-w-[140px]">
                 <ResolveAndDisplayReferrerIdentity
@@ -205,6 +231,14 @@ export function RegistrarActionCard({
                     namespaceId={namespaceId}
                     referral={referral}
                 />
+            </LabeledField>
+
+            <LabeledField fieldLabel="ENS Holiday Awards" className="w-[15%] min-w-[140px]">
+                <div className="w-fit h-[21px] flex flex-row flex-nowrap justify-start items-center gap-2">
+                    {/*TODO: Should the qualification be determined somehow else? What about potential bulk registration?*/}
+                    {referral.decodedReferrer === null ? <XIcon size={20} className="text-red-600" /> : <CheckIcon size={20} className="text-emerald-600"/>}
+                    <p className="text-black font-medium">{referral.decodedReferrer === null ? "Not qualified" : "Qualified"}</p>
+                </div>
             </LabeledField>
         </div>
     );
