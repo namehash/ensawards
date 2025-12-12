c7d78be6ad4060b4fd01cce4d0acffc607956fd2
Steve <contact@stevedylan.dev>
2025-12-12 09:48:33 -0500

    chore: updated list of tools

--- ensawards.org/src/data/bestPractices.ts
+++ ensawards.org/src/data/bestPractices.ts
@@ -88,10 +88,9 @@ export const displayNamedSmartContractsMainnet: BestPractice = {
         "Looking up the name of a smart contract on Ethereum mainnet uses the same process as looking up the name of any other account. " +
         "There are a variety of libraries capable of looking up the [primary name](https://docs.ens.domains/web/reverse) of a contract address according to all ENS best practices:\n" +
         "- [ensnode-sdk](https://github.com/namehash/ensnode/blob/main/packages/ensnode-sdk/README.md) (v1.0.0+)\n" +
-        "- [ensnode-react (v1.0.0+)](https://github.com/namehash/ensnode/blob/main/packages/ensnode-react/README.md)\n" +
-        "- [Viem (v2.35.0+)](https://viem.sh/docs/ens/actions/getEnsName#chain-specific-resolution)\n" +
-        "- [Wagmi (v2.18.0+)](https://wagmi.sh/react/api/hooks/useEnsName#chainid)\n" +
-        "- [Ethers (v6.4.1+)](https://docs.ethers.org/v6/api/providers/#Provider-lookupAddress)\n" + // TODO: Decide if we should include Ethers v5 as well
+        "- [ensnode-react](https://github.com/namehash/ensnode/blob/main/packages/ensnode-react/README.md) (v1.0.0+)\n" +
+        "- [Viem](https://viem.sh/docs/ens/actions/getEnsName#chain-specific-resolution) (v2.35.0+)\n" +
+        "- [Wagmi](https://wagmi.sh/react/api/hooks/useEnsName#chainid) (v2.18.0+)\n" +
         "Libraries and tools for additional languages or frameworks can be found in the [ENS documentation](https://docs.ens.domains/web/libraries/).",
     },
     sides: [
@@ -121,11 +120,10 @@ export const displayNamedSmartContractsL2: BestPractice = {
         "When users interact with a contract on a L2 chain, use the [ENSIP-19](https://docs.ens.domains/ensip/19) standard to lookup the primary name of the contract. " +
         "ENSIP-19 provides chain-specific primary names for L2 networks (including Optimism, Arbitrum, Base, Linea, and Scroll), with an automatic fallback to a default primary name (defined on mainnet) if no chain-specific primary name is defined. " +
         "There are several libraries to choose from that support ENSIP-19 and all ENS best practices: \n" +
-        "- [ensnode-sdk (v1.0.0+)](https://github.com/namehash/ensnode/blob/main/packages/ensnode-sdk/README.md)\n" +
-        "- [ensnode-react (v1.0.0+)](https://github.com/namehash/ensnode/blob/main/packages/ensnode-react/README.md)\n" +
-        "- [Viem (v2.35.0+)](https://viem.sh/docs/ens/actions/getEnsName#chain-specific-resolution)\n" +
-        "- [Wagmi (v2.18.0+)](https://wagmi.sh/react/api/hooks/useEnsName#chainid)\n" +
-        // Redacted Ethers until ENSIP-19 support arrives, already mentioned in displayNamedSmartContractsMainnet
+        "- [ensnode-sdk](https://github.com/namehash/ensnode/blob/main/packages/ensnode-sdk/README.md) (v1.0.0+)\n" +
+        "- [ensnode-react](https://github.com/namehash/ensnode/blob/main/packages/ensnode-react/README.md) (v1.0.0+)\n" +
+        "- [Viem](https://viem.sh/docs/ens/actions/getEnsName#chain-specific-resolution) (v2.35.0+)\n" +
+        "- [Wagmi](https://wagmi.sh/react/api/hooks/useEnsName#chainid) (v2.18.0+)\n" +
         "Libraries and tools for additional languages or frameworks can be found in the [ENS documentation](https://docs.ens.domains/web/libraries/).",
     },
     sides: [
