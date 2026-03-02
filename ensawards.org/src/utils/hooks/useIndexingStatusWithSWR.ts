1cd531fbfaf70ac84ddcc81b8ca99163e697c6f8
y3drk <ziebura.jedrzej@gmail.com>
2026-03-02 12:29:13 +0100

    Roll back the version update, due to prod API not being up-to-date with latest versions of packages + hooks updates

--- ensawards.org/src/utils/hooks/useIndexingStatusWithSWR.ts
+++ ensawards.org/src/utils/hooks/useIndexingStatusWithSWR.ts
@@ -1,3 +1,5 @@
+import { useNow } from "@namehash/namehash-ui";
+import { secondsToMilliseconds } from "date-fns";
 import { useCallback, useMemo } from "react";
 
 import {
@@ -9,16 +11,21 @@ import {
   type WithSDKConfigParameter,
 } from "@ensnode/ensnode-react";
 import {
-  type IndexingStatusRequest,
-  IndexingStatusResponseCodes,
-  type IndexingStatusResponseOk,
+  type CrossChainIndexingStatusSnapshotOmnichain,
+  createRealtimeIndexingStatusProjection,
+  type Duration,
+  type EnsApiIndexingStatusRequest,
+  EnsApiIndexingStatusResponseCodes,
+  type EnsApiIndexingStatusResponseOk,
 } from "@ensnode/ensnode-sdk";
 
-const DEFAULT_REFETCH_INTERVAL = 10 * 1000;
+const DEFAULT_REFETCH_INTERVAL = secondsToMilliseconds(10);
+
+const REALTIME_PROJECTION_REFRESH_RATE: Duration = 1;
 
 interface UseIndexingStatusParameters
-  extends IndexingStatusRequest,
-    QueryParameter<IndexingStatusResponseOk> {}
+  extends EnsApiIndexingStatusRequest,
+    QueryParameter<CrossChainIndexingStatusSnapshotOmnichain> {}
 
 /**
  * A proxy hook for {@link useIndexingStatus} which applies
@@ -29,6 +36,7 @@ export function useIndexingStatusWithSwr(
 ) {
   const { config, query = {} } = parameters;
   const _config = useENSNodeSDKConfig(config);
+  const now = useNow({ timeToRefresh: REALTIME_PROJECTION_REFRESH_RATE });
 
   const queryOptions = useMemo(() => createIndexingStatusQueryOptions(_config), [_config]);
   const queryKey = useMemo(() => ["swr", ...queryOptions.queryKey], [queryOptions.queryKey]);
@@ -38,24 +46,45 @@ export function useIndexingStatusWithSwr(
         // An indexing status response was successfully fetched,
         // but the response code contained within the response was not 'ok'.
         // Therefore, throw an error to avoid caching this response.
-        if (response.responseCode !== IndexingStatusResponseCodes.Ok) {
+        if (response.responseCode !== EnsApiIndexingStatusResponseCodes.Ok) {
           throw new Error(
             "Received Indexing Status response with responseCode other than 'ok' which will not be cached.",
           );
         }
 
-        // successful response to be cached
-        return response;
+        // The indexing status snapshot has been fetched and successfully validated for caching.
+        // Therefore, return it so that query cache for `queryOptions.queryKey` will:
+        // - Replace the currently cached value (if any) with this new value.
+        // - Return this non-null value.
+        return response.realtimeProjection.snapshot;
       }),
     [queryOptions.queryFn],
   );
 
+  // Call select function to `createRealtimeIndexingStatusProjection` each time
+  // `now` is updated.
+  const select = useCallback(
+    (cachedSnapshot: CrossChainIndexingStatusSnapshotOmnichain): EnsApiIndexingStatusResponseOk => {
+      const realtimeProjection = createRealtimeIndexingStatusProjection(cachedSnapshot, now);
+
+      // Maintain the original response shape of `IndexingStatusResponse`
+      // for the consumers. Creating a new projection from the cached snapshot
+      // each time `now` is updated should be implementation detail.
+      return {
+        responseCode: EnsApiIndexingStatusResponseCodes.Ok,
+        realtimeProjection,
+      } satisfies EnsApiIndexingStatusResponseOk;
+    },
+    [now],
+  );
+
   return useSwrQuery({
     ...queryOptions,
-    refetchInterval: query.refetchInterval ?? DEFAULT_REFETCH_INTERVAL, // Indexing status changes frequently
     ...query,
+    refetchInterval: query.refetchInterval ?? DEFAULT_REFETCH_INTERVAL, // Indexing status changes frequently
     enabled: query.enabled ?? queryOptions.enabled,
     queryKey,
     queryFn,
+    select,
   });
 }
