fa198f5eda08b6d2ee72edcb616a097472a9af7e
y3drk <ziebura.jedrzej@gmail.com>
2026-03-27 16:37:04 +0100

    Implement styling part of FI-0 from 03/27/26 Google Doc

--- ensawards.org/src/components/atoms/badges/ReferralProgramStatusBadge.tsx
+++ ensawards.org/src/components/atoms/badges/ReferralProgramStatusBadge.tsx
@@ -4,6 +4,7 @@ import {
 } from "@namehash/ens-referrals/v1";
 
 import { cn } from "@/utils/tailwindClassConcatenation.ts";
+import { breakLongWords } from "@/utils/textModifications";
 
 interface ReferralProgramStatusBadgeProps {
   status: ReferralProgramEditionStatusId;
@@ -12,10 +13,10 @@ interface ReferralProgramStatusBadgeProps {
 
 const getReferralProgramStatusStyle = (status: ReferralProgramEditionStatusId): string => {
   switch (status) {
-    case ReferralProgramEditionStatuses.AwardsReview: //TODO: Adjust to figma later
-      return "bg-yellow-500 text-white";
+    case ReferralProgramEditionStatuses.AwardsReview:
+      return "bg-amber-500 text-white";
     case ReferralProgramEditionStatuses.Scheduled:
-      return "bg-white text-black border border-[#E5E5E5]";
+      return "bg-white text-black border-[#E5E5E5]";
     case ReferralProgramEditionStatuses.Active:
       return "bg-emerald-600 text-white";
     case ReferralProgramEditionStatuses.Closed:
@@ -26,16 +27,16 @@ const getReferralProgramStatusStyle = (status: ReferralProgramEditionStatusId): 
 };
 
 export function ReferralProgramStatusBadge({ status, className }: ReferralProgramStatusBadgeProps) {
+  const userFacingStatusText = breakLongWords(status, /(?=[A-Z])/, 1).join(" ");
   return (
     <span
       className={cn(
-        "w-fit flex flex-row flex-nowrap justify-center items-center gap-[6px] px-2\n" +
-          "              py-0.5 rounded-full text-xs leading-normal font-semibold cursor-default",
+        "w-fit flex flex-row flex-nowrap justify-center items-center gap-[6px] px-2 py-0.5 rounded-full text-xs leading-normal font-semibold cursor-default border border-transparent",
         getReferralProgramStatusStyle(status),
         className,
       )}
     >
-      {status}
+      {userFacingStatusText}
     </span>
   );
 }
