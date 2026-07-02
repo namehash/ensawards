d3be0f78becd2a4475d302a036ec3549f35d216b
y3drk <ziebura.jedrzej@gmail.com>
2026-07-02 09:36:42 +0200

    Apply AI agents' suggestions, pt.1

--- ensawards.org/data/apps/xtcom-exchange/icon.tsx
+++ ensawards.org/data/apps/xtcom-exchange/icon.tsx
@@ -8,7 +8,7 @@ const Icon = ({ className, ...props }: React.SVGProps<SVGSVGElement>) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     viewBox="0 0 24 24"
-    fill="currentColor"
+    fill="none"
     className={cn("rounded-md", className)}
     {...props}
   >
